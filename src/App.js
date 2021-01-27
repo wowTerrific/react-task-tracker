ab46114cbedae0dfc5099cb700a22b4fba2fc993
Marshall <marshallstutzman@gmail.com>
2021-01-27 15:05:00 -0500

    Added 'add task' feature

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 const App = () => {
-
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
 	{
 		id: 1,
@@ -24,6 +25,12 @@ const App = () => {
 		reminder: false,
 	}
   ])
+  // Add Task
+  const addTask = (task) => {
+	const id = Math.floor(Math.random() * 10000) + 1;
+	const newTask = { id, ...task };
+	setTasks([...tasks, newTask]);
+  }
 
   // Delete Task
   const deleteTask = (id) => {
@@ -36,8 +43,10 @@ const App = () => {
 
   return (
     <div className="container">
-	<Header />
+	<Header onAdd={ () => setShowAddTask(!showAddTask) } showAdd={showAddTask} />
+	{ showAddTask ? <AddTask onAdd={addTask} /> : '' }
 	{ tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : <p>No Tasks to Show.</p> }
+
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,15 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-const Header = ({ title }) => {
-
-	const onClick = (e) => {
-		console.log(e);
-	}
+const Header = ({ title, onAdd, showAdd }) => {
 
 	return (
 		<header className='header'>
 			<h1>{title}</h1>
 			<Button 
-				color='green'
-				text='Add'
-				onClick={onClick}
+				color={!showAdd ? 'green' : 'red'}
+				text={!showAdd ? 'Add' : 'Close'}
+				onClick={onAdd}
 			/>
 		</header>
 	)
